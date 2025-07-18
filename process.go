8fa4028adf6dd801da342a58c6c13f8e528fd83b
CuculusBand <CuculusBand@outlook.com>
2025-07-18 23:25:22 +0800

    Add a RenamerProcessor struct to handle file renaming logic

--- process.go
+++ process.go
@@ -1,1 +1,22 @@
 package main
+
+import "os"
+
+// FileRenamer is a struct that holds the file processing logic
+type RenamerProcessor struct {
+	FolderPath    string        // FolderPath
+	Files         []os.FileInfo // All files in the folder
+	FilteredFiles []os.FileInfo // Files after filtering
+	FilterExt     string        // Extension filter
+	NewNames      []string      // New names for files
+	Prefix        string        // Prefix
+	Suffix        string        // Suffix
+	NewExtension  string        // New extension
+	RemovePrefix  bool          // Remove prefix
+	RemoveSuffix  bool          // Remove suffix
+}
+
+// Create new FileRenamer instance
+func NewFileProcessor() *RenamerProcessor {
+	return &RenamerProcessor{}
+}
